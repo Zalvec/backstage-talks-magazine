d0aeaf48203264e109785e0e3a992b584d9d130f
Roel <roel.van.bilzen@gmail.com>
2021-05-22 15:20:49 +0200

    Data input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
 import './scss/style.scss'
+import data from './data'
+import { v4 as uuid } from 'uuid'
 
+console.log()
 function App() {
   return (
-    <div className="App">
+    <>
+      <header>
+        {data.header}
+      </header>
+
+      <main>
+        {data.magazines.map( issue => 
+          <div className="cover">
+            <img src={issue.image} alt={issue.imageDescription} />
+            { issue.issuesRemaining > 0 ? (
+              <>
+                <p>Issue #{issue.issueNumber}</p>
+                <p>
+                  <a href={issue.buyLink}>BUY HERE</a>
+                </p>
+                <p>
+                  or in <a href={data.selectedStores}>selected stores</a>.
+                </p>
+              </>
+            ) : (
+              <>
+                <p>Issue #{issue.issueNumber} is sold out.</p>
+                <p>
+                  If you are lucky, you may get the last pieces in <a href={data.selectedStores}>selected stores</a>.
+                </p>
+              </>
+            )}
+            
+          </div>  
+        )}
+      </main>
       
-    </div>
+      <footer>
+        <p className="description">
+          {data.description.text}
+          <span className="copyright">
+            &copy; {data.description.copyright}
+            <a href={data.description.publisherLink} target="_blank" rel="noreferrer">
+              {data.description.publisher}
+            </a>
+            <a href={data.privacyPolicy} target="_blank" rel="noreferrer">
+              Privacy policy
+            </a>
+          </span>
+        </p>
+        <div className="info">
+          <p className="contact">
+            <a href={`mailto:` + data.email}>{data.email}</a>
+          </p>
+          <ul>
+            {data.magazines.map( issue => 
+              <li key={uuid()}>
+                <a href={`issue` + issue.issueNumber}>
+                  {`Issue #` + issue.issueNumber}
+                </a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,51 @@
+const data = {
+    "header": "Backstage talks",
+    "email": "info@backstagetalks.com",
+    "description": {
+        "text": "Backstage Talks is a magazine of casual, but in depth dialogues on design and business. Our decisions shape and influence this complex world-to have a chance to make the right ones, we need to talk.",
+        "copyright": "2021",
+        "publisher": "Published by studio Milk",
+        "publisherLink": "https://milk.sk/"
+    },
+    "selectedStores": "https://backstagetalks.com/",
+    "privacyPolicy": "https://backstagetalks.com/",
+    "magazines": [
+        {
+            "issueNumber": "5",
+            "image": "./images/backstagetalks_cover_issue_5.png",
+            "imageDescription": "Backstage talks Issue 5",
+            "issuesRemaining": "100",
+            "buyLink": "https://backstagetalks.com/"
+        },
+        {
+            "issueNumber": "4",
+            "image": "./images/backstagetalks_cover_issue_4.png",
+            "imageDescription": "Backstage talks Issue 4",
+            "issuesRemaining": "100",
+            "buyLink": "https://backstagetalks.com/"
+        },
+        {
+            "issueNumber": "3",
+            "image": "./images/backstagetalks_cover_issue_3.png",
+            "imageDescription": "Backstage talks Issue 3",
+            "issuesRemaining": "100",
+            "buyLink": "https://backstagetalks.com/"
+        },
+        {
+            "issueNumber": "2",
+            "image": "./images/backstagetalks_cover2017.png",
+            "imageDescription": "Backstage talks Issue 2",
+            "issuesRemaining": "100",
+            "buyLink": "https://backstagetalks.com/"
+        },
+        {
+            "issueNumber": "1",
+            "image": "./images/backstagetalks_cover2016_n.png",
+            "imageDescription": "Backstage talks Issue 1",
+            "issuesRemaining": "0",
+            "buyLink": "https://backstagetalks.com/"
+        },
+    ]
+}
+
+export default data
